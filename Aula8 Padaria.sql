e412a625afb3a2590bc32246598d68d036c3b905
tamarambsilva <tamara_tmb@hotmail.com>
2025-10-14 21:44:28 -0300

    ATIVIDADE AULA 12

--- Aula8 Padaria.sql
+++ Aula8 Padaria.sql
@@ -11,13 +11,6 @@ id INT PRIMARY KEY AUTO_INCREMENT,
 nome VARCHAR(150) NOT NULL
 );
 
-CREATE TABLE Aula008_VENDAS (
-  id INT PRIMARY KEY AUTO_INCREMENT,
-  id_produto INT NOT NULL,
-  CONSTRAINT fk_Venda_Produto FOREIGN KEY (id_produto) REFERENCES Aula008_PRODUTO(id),
-  quantidade INT NOT NULL,
-  data_venda DATE
-);
 
 INSERT INTO Aula008_PRODUTO (nome, preco_un)
 VALUES 
@@ -29,35 +22,27 @@ VALUES
 INSERT INTO Aula008_FORNECEDOR (nome)
 VALUES ("Fornecedor A"), ("Fornecedor B"), ("Fornecedor C");
 
-INSERT INTO Aula008_VENDAS (id_produto, quantidade, data_venda)
+  
+  
+  INSERT INTO Aula008_PRODUTO (nome, preco_un)
 VALUES 
-  (1, 1, '2025-10-08'), 
-  (1, 2, '2025-10-08'), 
-  (2, 1, '2025-10-08'), 
-  (2, 3, '2025-10-08'), 
-  (3, 1, '2025-10-08'), 
-  (3, 5, '2025-10-08');
+  ('pudim', 1.50), 
+  ('Pao', 1.00), 
+  ('Bolo Recheado', 6.00),
+  ('Pao de Queijo', 1.50);
   
-  SELECT * FROM Aula008_PRODUTO;
-  
-  SELECT id_produto, SUM(quantidade)
-  FROM Aula008_VENDAS
-  GROUP BY id_produto;
-  
- SELECT SUM(v.quantidade) AS total_vendas_fuba
-FROM Aula008_VENDAS v
-JOIN Aula008_PRODUTO p ON v.id_produto = p.id
-WHERE p.nome = 'Bolo Fuba';
 
-SELECT SUM(v.quantidade) AS total_vendas_chocolate
-FROM Aula008_VENDAS v
-JOIN Aula008_PRODUTO p ON v.id_produto = p.id
-WHERE p.nome = 'Bolo Chocolate';
+UPDATE Aula008_PRODUTO
+SET preco_un = 1.20
+WHERE id = 2;
 
-SELECT p.nome, v.quantidade, v.data_venda
-FROM Aula008_VENDAS v
-JOIN Aula008_PRODUTO p ON v.id_produto = p.id
-WHERE v.data_venda = '2025-10-08';
+SELECT * FROM Aula008_PRODUTO;
+
+DELETE FROM Aula008_PRODUTO
+WHERE id = 1;
+
+SELECT * FROM Aula008_PRODUTO;
+
 
 
   
